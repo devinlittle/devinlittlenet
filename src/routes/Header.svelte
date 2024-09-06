5eecb9f205a8d91bad241344bb3f8766c7e14e6f
Devin Little <pixlovesit@gmail.com>
2024-09-06 18:11:01 -0400

    Updt gh username

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -19,7 +19,7 @@
 	</nav>
 
 	<div class="corner">
-		<a href="https://github.com/pixthehe/devinlittlenet">
+		<a href="https://github.com/devinlittle/devinlittlenet">
 			<img src={github} alt="GitHub" />
 		</a>
 	</div>
